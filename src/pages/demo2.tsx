ceca7fff00d5ff7dc5825c01e747cfbdae2b975e
shenck <shenck1807@joyowo.com>
2024-03-04 17:57:07 +0800

    :bug: 修复demo2报错
    - 因通过new URL本地引用.wasm文件时没有使用正确的 MIME 类型，改为引用外链，后续按此方式使用后可以将wasm文件放在服务上，或者用bunlder模式打包，不用web模式

--- src/pages/demo2.tsx
+++ src/pages/demo2.tsx
@@ -3,7 +3,7 @@ import initWasm,{ add } from "../../../rust2wasm-rust/web/rust_demo";
 
 export default function Demo2() {
 	const init = useCallback(async () => {
-		await initWasm();
+		await initWasm('http://192.168.120.102:8000/rust_demo_bg.wasm');
 		console.log(add(1, 2));
 	}, []);
 
